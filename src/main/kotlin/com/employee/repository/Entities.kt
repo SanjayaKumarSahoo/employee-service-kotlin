cc77a6c27b4ae9bb934643ba77205eed0b2a67ba
sahoo.skumar@gmail.com <Sushama#2621>
2021-04-08 15:47:44 +0530

    [N/A] : adding objectmapper

--- src/main/kotlin/com/employee/repository/Entities.kt
+++ src/main/kotlin/com/employee/repository/Entities.kt
@@ -1,59 +1,56 @@
 package com.employee.repository
 
-import java.util.Date
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
-import javax.persistence.Table
-import javax.persistence.Temporal
-import javax.persistence.TemporalType
+import java.util.*
+import javax.persistence.*
 
 
 @Entity
 @Table(name = "EMP", catalog = "SCOTT")
 class Employee(
-    @Column(name = "ENAME")
-    var ename: String,
+        @Column(name = "ENAME")
+        var ename: String,
 
-    @Column(name = "JOB")
-    var job: String,
+        @Column(name = "JOB")
+        var job: String,
 
-    @Column(name = "MGR")
-    var mgr: Int,
+        @Column(name = "MGR")
+        var mgr: Int,
 
-    @Temporal(TemporalType.DATE) @Column(name = "HIREDATE")
-    var hireDate: Date,
+        @Temporal(TemporalType.DATE) @Column(name = "HIREDATE")
+        var hireDate: Date,
 
-    @Column(name = "SAL")
-    var sal: Double,
+        @Column(name = "SAL")
+        var sal: Double,
 
-    @Column(name = "COMM")
-    var comm: Double,
+        @Column(name = "COMM")
+        var comm: Double,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    var department: Department,
+        @ManyToOne(fetch = FetchType.LAZY)
+        var department: Department,
 
-    @Id @GeneratedValue @Column(name = "EMPNO")
-    var empno: Long? = null
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "emp_generator")
+        @SequenceGenerator(name = "emp_generator", sequenceName = "emp_seq")
+        @Column(name = "EMPNO")
+        var empno: Long? = null
 )
 
 @Entity
 @Table(name = "DEPT", catalog = "SCOTT")
 class Department(
-    @Id @GeneratedValue @Column(name = "DEPTNO")
-    var deptNo: Long? = null,
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dept_generator")
+        @SequenceGenerator(name = "dept_generator", sequenceName = "dept_seq")
+        @Column(name = "DEPTNO")
+        var deptNo: Long? = null,
 
-    @Column(name = "DEPT_NAME")
-    var dName: String,
+        @Column(name = "DEPT_NAME")
+        var dName: String,
 
-    @Column(name = "LOCATION")
-    var loc: String,
+        @Column(name = "LOCATION")
+        var loc: String,
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "department")
-    var employees: MutableList<Employee> = arrayListOf()
+        @OneToMany(fetch = FetchType.LAZY, mappedBy = "department")
+        var employees: MutableList<Employee> = arrayListOf()
 )
 
